cc64f5cd52617edffe298f7eecd292971a6ade56
krististina <72921114+krististina@users.noreply.github.com>
2020-12-15 19:22:44 +0100

    lalaaa nog niet klaar, ben nog bezig

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
+    var slides = document.getElementsByClassName("slides");
     var dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
@@ -29,7 +29,7 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace("active", "");
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
